2c6c593f4e42f2b4939e70a79afb9bd8a2371176
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-11-17 15:41:52 -0600

    created the HomeController with a GetMapping for the landing page

---
+++ src/main/java/com/example/springblog/FirstController.java
@@ -0,0 +1,29 @@
+package com.example.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class FirstController
+{
+    @GetMapping("/test")
+    @ResponseBody
+    public String test()
+    {
+        return "Hello from Spring";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/bubbles")
+    @ResponseBody
+    public String bubbles()
+    {
+        return "bbububub!";
+    }
+
+    @GetMapping("/greeting/{name}/{lastName}")
+    @ResponseBody
+    public String greeting(@PathVariable String name, @PathVariable String lastName)
+    {
+        return "Greetings " + name + " " + lastName;
+    }
+}
