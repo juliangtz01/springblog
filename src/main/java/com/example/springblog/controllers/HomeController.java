d61fe24e3cc7097d2e7e85f4f586967ffe9fa98f
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-11-18 16:03:53 -0600

    working on the lecture

--- src/main/java/com/example/springblog/controllers/HomeController.java
+++ src/main/java/com/example/springblog/controllers/HomeController.java
@@ -1,16 +1,37 @@
 package com.example.springblog.controllers;
 
+import com.example.springblog.models.Ad;
+import com.example.springblog.repositories.AdRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 
 @Controller
 public class HomeController
 {
+    private final AdRepository adsDao;
+    public HomeController(AdRepository adsDao)
+    {
+        this.allAds = adsDao;
+    }
+
     @GetMapping("/")
     @ResponseBody
     public String home()
     {
         return "This is the landing page!";
+    }
+
+    @GetMapping("/ads")
+    public String allAds(Model model)
+    {
+        List<Ad> ads = adsDao.findAll();
+
+        model.addAttribute("ads", ads);
+        return "ads";
     }
 }
---
+++ src/main/java/com/example/springblog/repositories/AdRepository.java
@@ -0,0 +1,9 @@
+package com.example.springblog.repositories;
+
+import com.example.springblog.models.Ad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdRepository extends JpaRepository<Ad, Long>
+{
+
+}
