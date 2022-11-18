6a7a6c40a03f5c7dd2be44980a9ac4dc6064d2ac
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-11-18 11:06:55 -0600

    working on the lecture

--- src/main/java/com/example/springblog/HomeController.java
+++ src/main/java/com/example/springblog/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController
@@ -12,5 +17,16 @@ public class HomeController
     public String home()
     {
         return "This is the landing page!";
+    }
+
+    @GetMapping("/dogPark")
+    public String dogPark(Model model)
+    {
+        List<String> dogs = new ArrayList<>();
+        dogs.add("Bubbles");
+        dogs.add("Spot");
+        dogs.add("Ezekial");
+        model.addAttribute("dogs", dogs);
+        return "dogPark";
     }
 }
